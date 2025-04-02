7371e6e9c054a80eb853b24579a87e45d35a4188
Joe George <127773439+joegeorge022@users.noreply.github.com>
2025-04-02 19:38:50 +0530

    .

--- js/chatbot.js
+++ js/chatbot.js
@@ -1,5 +1,12 @@
 const chatbot = {
   init: function() {
+    if (!document.querySelector('link[href*="font-awesome"]')) {
+      const fontAwesome = document.createElement('link');
+      fontAwesome.rel = 'stylesheet';
+      fontAwesome.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css';
+      document.head.appendChild(fontAwesome);
+    }
+    
     this.createChatbotUI();
     this.bindEvents();
   },
