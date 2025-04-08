29372d9178300bfe16e83f3b8b30230d167708bc
Ananthakrishnan K V <ananthakrishnanvenu007@gmail.com>
2025-04-08 19:45:20 +0530

    Update script.js

--- Exp-2/script.js
+++ Exp-2/script.js
@@ -8,17 +8,27 @@ document.addEventListener("DOMContentLoaded", function () {
         const password = form.password.value.trim();
         let isValid = true;
         
-        if (!validateEmail(email)) {
+        if (email === "") {
+            alert("Email is required.");
+            isValid = false;
+        }
+
+        else if (!validateEmail(email)) {
            alert("Please enter a valid email address.");
             isValid = false;
         }
 
-        
-        if (password.length < 6) {
+        if (password === "") {
+            alert("Password is required.");
+            isValid = false;
+        }
+
+        else if (password.length < 6) {
             alert("Password must be at least 6 characters long.");
             isValid = false;
         }
 
+        
         if (isValid) {
             alert("Login successful!");
             form.submit();
